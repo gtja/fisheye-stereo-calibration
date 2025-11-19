57445a42978c6265e1f64ae8a6701753e38b00b6
165697115@qq.com <165697115@qq.com>
2025-11-19 17:56:04 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git pull
 docker build --memory 4g --memory-swap 4g -t fisheye-stereo-calibration .
-docker run -e CALIBRATION_MODEL=double_sphere -e CALIBRATION_WORKFLOW=hand-eye -v $(pwd)/imgs2:/data/imgs fisheye-stereo-calibration -w 11 -h 8 -s 0.025 -d /data/imgs/ -l left -r right -e bmp -b 0.06
+docker run -e CALIBRATION_MODEL=double_sphere -e CALIBRATION_WORKFLOW=hand_eye -v $(pwd)/imgs2:/data/imgs fisheye-stereo-calibration -w 11 -h 8 -s 0.025 -d /data/imgs/ -l left -r right -e bmp -b 0.06
